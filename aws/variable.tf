32ca16fb98c3041b48867de7e30695387ef3af0a
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-07-08 21:20:11 +0700

    chores: add backend storage module variable

--- aws/variable.tf
+++ aws/variable.tf
@@ -32,3 +32,13 @@ variable "enable_nat_gateway" {
   description = "Enable NAT Gateway"
   type        = bool
 }
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to store Terraform state files"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+}
