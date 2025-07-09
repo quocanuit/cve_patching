b5332a8fbbe1cd899039db8f65b2d2e740aede46
Nguyễn Quốc An <quocanuit@gmail.com>
2025-07-09 09:53:50 +0700

    Revert "feat: add aws infras (#14)"
    This reverts commit 87d496e097c31eff7833cd3606ad90e244018c2a.

--- aws/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "challenge8-state-storage"
-#     key            = "jenkins/terraform.tfstate"
-#     region         = "ap-southeast-2"
-#     use_lockfile   = true
-#     dynamodb_table = "challenge8-terraform-lock"
-#   }
-# } 
--- aws/modules/storage/backend/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket to store Terraform state files"
-  type        = string
-}
-
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for state locking"
-  type        = string
-}
